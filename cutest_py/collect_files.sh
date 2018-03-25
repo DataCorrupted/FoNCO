c277b0236eedf65d3138bbba4e277b4a05c64db9
Peter Rong <PeterRong96@gmail.com>
2018-03-25 22:02:40 +0800

    changed a script. It wouldn't run on apple.
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- cutest_py/collect_files.sh
+++ cutest_py/collect_files.sh
@@ -2,4 +2,4 @@
 [ -z "$MYARCH" ] && echo "Environment MYARCH not set. Have you installed CUTEst correctly?" && exit 1;
 
 cp $CUTEST/objects/$MYARCH/double/libcutest.a .
-cp $CUTEST/modules . -rf
+cp -rf $CUTEST/modules . 
