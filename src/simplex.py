dc5df0ae59476353e1082374c9ca5d7124e8f628
Peter Rong <PeterRong96@gmail.com>
2018-04-11 09:57:55 +0800

    simplex class almost done. waiting for dual.
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- src/simplex.py
+++ src/simplex.py
@@ -8,7 +8,7 @@ def pause():
 class Simplex:
 	# Check if the input is legal. Don't worry about it if you
 	# are not familiar with numpy's API.
-	def inputCheck(self, c, A, b, basis):
+	def inputCheck_(self, c, A, b, basis):
 		# Check if obj: cx, constrain Ax = b is of type np.
 		if  type(c) != np.ndarray or \
 			type(A) != np.ndarray or \
@@ -31,52 +31,52 @@ class Simplex:
 	#			will be reset to np.arange(m-n, m), as the easiest solution 
 	# 			usally falls there.
 	def __init__(self, c, A, b, basis = None):
-		self.inputCheck(c, A, b, basis)
+		self.inputCheck_(c, A, b, basis)
 		m, n = A.shape
-		self.n, self.m = (n, m)
-		self.c = c;
-		self.tableau = np.concatenate((A, b), axis=1);
+		self.n_, self.m_ = (n, m)
+		self.c_ = c;
+		self.tableau_ = np.concatenate((A, b), axis=1);
 		if basis is None:
-			self.basis = np.arange(m-n, m)
+			self.basis_ = np.arange(m-n, m)
 		else:
-			self.basis = basis
+			self.basis_ = basis
+		self.check_ = self.zSubC_();
 
 	# Used to update c and A, as they will change with rho.
-	def updateC(c):	self.c = c
+	def updateC(c):	self.c_ = c
 	def updateA(A):	self.A = A
 
 	# input: none
 	# return: 
 	# 		ndarray with size (1, n)(a vector) indicating z-c
-	def zSubC(self):
+	def zSubC_(self):
 		# Compute zj-cj. If zj - cj >= 0 for all columns then current 
 		# solution is optimal solution.
-		A = self.tableau[:, 0: self.n]
-		cb = np.reshape(self.c[list(basis)], (self.m, 1))
+		A = self.tableau_[:, 0: self.n_]
+		cb = np.reshape(self.c_[list(basis)], (self.m_, 1))
 		z = np.sum(cb * A, axis = 0)
 		# Leave this for the sake of debug
-		print(cb)
-		print(A)
-		print(cb * A)
-		print(z)
-		print(z - self.c)
-		pause()
-		return z - self.c
+		return z - self.c_
+
+	# input: none
+	# output:
+	# 		boolean, determining if the problem is in optimal now.
+	def isOptimal_(self):
+		return np.all(self.check_ <= 0)
 
 	# input: 
 	# 		verbose: boolean, determing whether to print debug information.
 	# return: none
 	def solve(self, verbose = False):
-		tableau = self.tableau
+		tableau = self.tableau_
 		# Start the simplex algorithm:
-		check = self.zSubC()
 		count = 0
 
-		while ~np.all(check >= 0):
+		while not self.isOptimal_():
 			# Determine the pivot column:
 			# The pivot column is the column corresponding to 
 			# minimum zj-cj.
-			pivot_col = np.argmin(check)
+			pivot_col = np.argmax(self.check_)
 			# Determine the positive elements in the pivot column.
 			# If there are no positive elements in the pivot column  
 			# then the optimal solution is unbounded.
@@ -85,26 +85,27 @@ class Simplex:
 				print('Unbounded Solution!')
 				break
 			# Determine the pivot row:
-			divide=(tableau[positive_rows, self.n]
+			divide=(tableau[positive_rows, self.n_]
 				/tableau[positive_rows,pivot_col])
 			pivot_row = positive_rows[np.where(divide 
 				== divide.min())[0][-1]]
 			# Update the basis:
-			self.basis[pivot_row] = pivot_col
+			self.basis_[pivot_row] = pivot_col
 			# Perform gaussian elimination to make pivot element one and
 			# elements above and below it zero:
 			tableau[pivot_row,:]=(tableau[pivot_row,:]
 				/tableau[pivot_row,pivot_col])
-			for row in range(self.m):
+			for row in range(self.m_):
 				if row != pivot_row:
 					tableau[row,:] = (tableau[row,:] 
 						- tableau[row,pivot_col]*tableau[pivot_row,:])
-			# Determine zj-cj
-			check = self.zSubC();
-			count += 1
 			if verbose:
 				print('Step %d' % count)
 				print(tableau)
+				pause()
+			# Determine zj-cj
+			count += 1
+			self.check_ = self.zSubC_();
 
 
 	def getSolution(self):
@@ -113,12 +114,12 @@ class Simplex:
 		# will return almost instantly.
 		self.solve()
 		# Get the no of columns in the tableau:
-		n_cols = self.tableau.shape[1]
+		n_cols = self.tableau_.shape[1]
 		# Get the optimal solution:
-		solution = np.zeros(self.c.size)
-		solution[list(self.basis)] = self.tableau[:,n_cols-1]
+		solution = np.zeros(self.c_.size)
+		solution[list(self.basis_)] = self.tableau_[:,n_cols-1]
 		# Determine the optimal value:
-		value = np.sum(self.c[list(self.basis)] * self.tableau[:,n_cols-1])
+		value = np.sum(self.c_[list(self.basis_)] * self.tableau_[:,n_cols-1])
 		return solution,value
 
 	# Return dual based on primal solution.
@@ -142,19 +143,7 @@ if __name__ == "__main__":
 		return c, A, b, basis
 
 	def Q2():
-		# Define A, b:
-		A = np.array([
-			[1, 1, 1,  1,  1, 0, 0],
-			[7, 5, 3,  2,  0, 1, 0],
-			[3, 5, 10, 15, 0, 0, 1]
-		], dtype = np.float64)
-		b = np.array([[15], [120], [100]], dtype = np.float64)
-		# Define the objective function and the initial basis:
-		c = np.array([4, 5, 9, 11, 0, 0, 0])
-		basis = np.array([4, 5, 6])
-		return c, A, b, basis
-
-	def Q3():
+		# Check this example on pp.46
 		# Define A, b:
 		A = np.array([
 			[ 1,  1, -2, 1, 0, 0],
@@ -163,14 +152,14 @@ if __name__ == "__main__":
 		], dtype = np.float64)
 		b = np.array([[10], [8], [4]], dtype = np.float64)
 		# Define the objective function and the initial basis:
-		c = np.array([-1, 2, -1, 0, 0, 0])
+		c = np.array([1, -2, 1, 0, 0, 0])
 		basis = np.array([3, 4, 5])
 		return c, A, b, basis
 
-	c, A, b, basis = Q1();
+	c, A, b, basis = Q2();
 	# Run the simplex algorithm:
 	s = Simplex(c, A, b, basis);
-	s.solve()
+	s.solve(verbose = True)
 	# Get the optimal soultion:
 	optimal_solution,optimal_value = s.getSolution()
 	# Print the results:
